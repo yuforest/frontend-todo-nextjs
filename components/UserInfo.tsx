601d19126a7c8d6088ca9a2c2f192deecd0738fd
大森優太 <>
2022-09-09 15:05:45 +0900

    feat: first commit

---
+++ components/UserInfo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useQueryUser } from '../hooks/useQueryUser'
+import { Loader } from '@mantine/core'
+
+export const UserInfo = () => {
+  const { data: user, status } = useQueryUser()
+  if (status === 'loading') return <Loader />
+  return <p>{user?.email}</p>
+}
